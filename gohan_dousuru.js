c90ac51fd818e75b155de296090fef066675a9d3
rei620m <96822858+rei620m@users.noreply.github.com>
2024-09-22 16:40:41 +0900

    Update gohan_dousuru.js

--- gohan_dousuru.js
+++ gohan_dousuru.js
@@ -3,14 +3,33 @@ const selectedItemsInput = document.getElementById('selected_items');
 const mainSelect = document.getElementById('main');
 const sideSelect = document.getElementById('side'); 
 const soupSelect = document.getElementById('soup'); 
+const dateInput = document.getElementById('date');
 
 const options = {
+    main: {
     "ピーマン": ["ピーマン肉詰め", "ガパオライス", "ファヒータ"],
     "キャベツ": ["ロールキャベツ", "焼うどん", "焼きそば"],
-    "玉ねぎ": ["親子丼", "玉ねぎ焼き", "牛皿", "肉豆腐"],
-    "人参": ["カレーライス", "焼うどん", "焼きそば", "きんぴらごぼう"],
-    "セロリ": ["フェジョアーダ", "ミネストローネ"],
-    "レタス": ["タコス", "レタスと韓国のりのサラダ"],
+    "玉ねぎ": ["親子丼"],
+    "人参": ["カレーライス", "焼うどん", "焼きそば"],
+    "セロリ": ["フェジョアーダ"],
+    "レタス": ["タコス"],
+    },
+    side: {
+    "ピーマン": [],
+    "キャベツ": [],
+    "玉ねぎ": ["玉ねぎ焼き", "牛皿", "肉豆腐"],
+    "人参": ["きんぴらごぼう"],
+    "セロリ": [],
+    "レタス": ["レタスと韓国のりのサラダ"],
+    },
+    soup: {
+    "ピーマン": [],
+    "キャベツ": [],
+    "玉ねぎ": [],
+    "人参": [],
+    "セロリ": ["ミネストローネ"],
+    "レタス": [],
+    },
 };
 
 tiles.forEach(tile => {
@@ -56,3 +75,13 @@ function createOption(optionText, highlight) {
     }
     return option;
 }
+
+function setDefaultDate() {
+    const today = new Date();
+    const options = { timeZone: 'Asia/Hong_Kong', year: 'numeric', month: '2-digit', day: '2-digit' };
+    const formatter = new Intl.DateTimeFormat('en-CA', options);
+    const [year, month, day] = formatter.format(today).split('-');
+    dateInput.value = `${year}-${month}-${day}`;
+}
+
+setDefaultDate();
