9deb937808c58c02b8be58deb3ffea8414ca4120
rei620m <96822858+rei620m@users.noreply.github.com>
2024-09-28 21:07:05 +0900

    highlight pink

--- gohan_dousuru.js
+++ gohan_dousuru.js
@@ -62,7 +62,7 @@ document.addEventListener('DOMContentLoaded', () => {
     function highlightSelectOptions(selectElement, itemsToHighlight) {
         Array.from(selectElement.options).forEach(option => {
             if (itemsToHighlight && itemsToHighlight.includes(option.value)) {
-                option.style.backgroundColor = '#e6ffe6';
+                option.style.backgroundColor = '#ffd3da';
             }
         });
     }
