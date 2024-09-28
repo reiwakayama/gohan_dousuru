036a7bb4f0d99e72273a3eb29d5a0331dc3694b6
rei620m <rei-morikawa@uwalumni.com>
2024-09-29 00:04:16 +0900

    added js automation

--- gohan_dousuru.py
+++ gohan_dousuru.py
@@ -4,7 +4,6 @@ locale.setlocale(locale.LC_ALL, 'ja_JP.UTF-8')
 
 df = pd.read_excel("C:\\Users\\81701\\gohan_dousuru\\gohan_dousuru.xlsx")
 
-"""
 options_grouping = {
     'main': sorted(df[df['type'] == 'main']['menu'].tolist(), key=locale.strxfrm), 
     'side': sorted(df[df['type'] == 'side']['menu'].tolist(), key=locale.strxfrm), 
@@ -26,31 +25,18 @@ gohan_dousuru_html = gohan_dousuru_html.replace('<!-- SOUP_OPTIONS_PLACEHOLDER -
 
 with open('C:\\Users\\81701\\gohan_dousuru\\gohan_dousuru.html', 'w', encoding='utf-8') as file:
     file.write(gohan_dousuru_html)
-"""
 
-piman_menus = df[df['ingredient'].str.contains('ピーマン', case=False, na=False)]
-piman_menus = piman_menus['menu'].tolist()
-piman_menus = ', '.join(f'"{menu}"' for menu in piman_menus)
+def get_menus(ingredient):
+    menus = df[df['ingredient'].str.contains(ingredient, case=False, na=False)]['menu'].tolist()
+    return ', '.join(f'"{menu}"' for menu in menus)
 
-cabbage_menus = df[df['ingredient'].str.contains('キャベツ', case=False, na=False)]
-cabbage_menus = cabbage_menus['menu'].tolist()
-cabbage_menus = ', '.join(f'"{menu}"' for menu in cabbage_menus)
-
-onion_menus = df[df['ingredient'].str.contains('玉ねぎ', case=False, na=False)]
-onion_menus = onion_menus['menu'].tolist()
-onion_menus = ', '.join(f'"{menu}"' for menu in onion_menus)
-
-carrot_menus = df[df['ingredient'].str.contains('人参', case=False, na=False)]
-carrot_menus = carrot_menus['menu'].tolist()
-carrot_menus = ', '.join(f'"{menu}"' for menu in carrot_menus)
-
-celery_menus = df[df['ingredient'].str.contains('セロリ', case=False, na=False)]
-celery_menus = celery_menus['menu'].tolist()
-celery_menus = ', '.join(f'"{menu}"' for menu in celery_menus)
-
-lettuce_menus = df[df['ingredient'].str.contains('レタス', case=False, na=False)]
-lettuce_menus = lettuce_menus['menu'].tolist()
-lettuce_menus = ', '.join(f'"{menu}"' for menu in lettuce_menus)
+# Get the menus for each ingredient
+piman_menus = get_menus('ピーマン')
+cabbage_menus = get_menus('キャベツ')
+onion_menus = get_menus('玉ねぎ')
+carrot_menus = get_menus('人参')
+celery_menus = get_menus('セロリ')
+lettuce_menus = get_menus('レタス')
 
 with open("C:\\Users\\81701\\gohan_dousuru\\gohan_dousuru_template.js", 'r', encoding='utf-8') as file:
     template_js = file.read()
@@ -62,5 +48,5 @@ gohan_dousuru_js = gohan_dousuru_js.replace('/*CARROT_MENUS_PLACEHOLDER*/', carr
 gohan_dousuru_js = gohan_dousuru_js.replace('/*CELERY_MENUS_PLACEHOLDER*/', celery_menus)
 gohan_dousuru_js = gohan_dousuru_js.replace('/*LETTUCE_MENUS_PLACEHOLDER*/', lettuce_menus)
 
-with open('C:\\Users\\81701\\gohan_dousuru\\gohan_dousuru_test.js', 'w', encoding='utf-8') as file:
+with open('C:\\Users\\81701\\gohan_dousuru\\gohan_dousuru.js', 'w', encoding='utf-8') as file:
     file.write(gohan_dousuru_js)
